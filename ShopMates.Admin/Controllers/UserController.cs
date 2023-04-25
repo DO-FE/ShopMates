5bd1e46eed540cd942f8aadc360e0a88378a9878
axyres <prodesaxy@gmail.com>
2023-04-25 11:43:54 +0700

    Add Web Admin Manager

---
+++ ShopMates.Admin/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopMates.ViewModels.System.Users;
+
+namespace ShopMates.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
