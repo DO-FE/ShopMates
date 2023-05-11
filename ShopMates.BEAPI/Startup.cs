b2f08561ee80719c80b8e35b44f3e8da2184a081
axyres <prodesaxy@gmail.com>
2023-05-11 12:45:43 +0700

    Update Admin View

--- ShopMates.BEAPI/Startup.cs
+++ ShopMates.BEAPI/Startup.cs
@@ -131,7 +131,7 @@ namespace ShopMates.BEAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseAuthentication();
