22602bae150d66e7a671b95341c9461e7a810f5d
axyres <prodesaxy@gmail.com>
2023-08-03 23:52:12 +0700

    Update Admin

--- ShopMates.Integration/LanguageApiClient.cs
+++ ShopMates.Integration/LanguageApiClient.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using ShopMates.Data.Entities;
+using ShopMates.Utilities.Constants;
 using ShopMates.ViewModels.Common;
 using ShopMates.ViewModels.System.Languages;
 using System;
@@ -13,8 +14,10 @@ namespace ShopMates.Integration
 {
     public class LanguageApiClient : BaseApiClient, ILanguageApiClient
     {
+        private readonly IConfiguration _configuration;
         public LanguageApiClient(IHttpClientFactory httpClientFactory,IHttpContextAccessor httpContextAccessor, IConfiguration configuration) : base(httpClientFactory, httpContextAccessor, configuration)
         {
+            _configuration = configuration;
         }
 
         public async Task<APIResult<List<LanguageViewModel>>> GetAll()
